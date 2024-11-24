cd2948f5f9327ec32020df67549f0c56a40172e7
DavidChisenga11 <davidchisenga09@gmail.com>
2024-11-24 15:23:25 +0200

    First commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <h3>Hey {{name}}</h3>
+    <h3 class="david">Hey {{id}}</h3>
+  </div>
+</template>
+
+<script>
+  export default{
+    data(){
+      return{
+        name: 'David M Chisenga',
+        id: '1905259'
+      }
+    }
+  }
+</script>
+
+
+<style>
+  .david{
+    color: green;
+  }
+</style>
